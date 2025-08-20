8cddef9d5843411293c75cfb7de9eacce6aafa12
agordienproject <agordien.project@gmail.com>
2025-08-20 11:35:36 +0200

    Change /api route place in the code

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -20,14 +20,14 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization', 'Accept']
   }));
 
-app.use("/api", (req, res, next) => {
-  console.log(`API Request: ${req.method} ${req.originalUrl}`);
-  res.status(200).json({ message: "API Request Logged" });
-});
 app.use("/api/auth", authRoutes);
 app.use("/api/inspections", inspectionRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/dashboards", dashboardRoutes);
 
-
-export default app;
+app.use("/api", (req, res, next) => {
+  console.log(`API Request: ${req.method} ${req.originalUrl}`);
+  res.status(200).json({ message: "API Request Logged" });
+});
+  
+  export default app;
