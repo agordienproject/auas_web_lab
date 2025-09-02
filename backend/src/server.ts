b68055a902db2484347d5332aff820507a2df224
agordienproject <agordien.project@gmail.com>
2025-09-02 11:41:23 +0200

    remove .env and add templates

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,7 +5,7 @@ import { config } from "dotenv";
 config();
 
 const PORT = Number(process.env.PORT_SERVEUR) || 3000;
-const GLOBAL_IP = process.env.GLOBAL_IP || "192.168.3.1";
+const GLOBAL_IP = process.env.GLOBAL_IP || "127.0.0.1";
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Backend running on port ${PORT}. http://${GLOBAL_IP}:${PORT}`);
